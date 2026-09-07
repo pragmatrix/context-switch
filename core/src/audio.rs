3c1c6aee8ff0f3ba83e7fd71195e8a3bffb6d11a
Armin Sander <armin@replicator.org>
2026-09-07 13:06:17 +0200

    core: use as_chunks in from_le_bytes and add tests

--- core/src/audio.rs
+++ core/src/audio.rs
@@ -28,8 +28,10 @@ pub fn to_le_bytes(audio: impl AsRef<[i16]>) -> Vec<u8> {
 pub fn from_le_bytes(audio: impl AsRef<[u8]>) -> Vec<i16> {
     audio
         .as_ref()
-        .chunks_exact(2)
-        .map(|chunk| i16::from_le_bytes([chunk[0], chunk[1]]))
+        .as_chunks::<2>()
+        .0
+        .iter()
+        .map(|&chunk| i16::from_le_bytes(chunk))
         .collect()
 }
 
@@ -42,4 +44,32 @@ pub fn chunk_8192(audio: Vec<u8>) -> Vec<Vec<u8>> {
         .chunks(MAX_CHUNK_SIZE)
         .map(|chunk| chunk.to_vec())
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_le_bytes_decodes_samples() {
+        let samples = from_le_bytes([0x00, 0x00, 0x00, 0x80, 0xff, 0x7f]);
+        assert_eq!(samples, vec![0, i16::MIN, i16::MAX]);
+    }
+
+    #[test]
+    fn from_le_bytes_round_trips_to_le_bytes() {
+        let samples = vec![0i16, 42, -1, 32000, -32000, i16::MAX, i16::MIN];
+        assert_eq!(from_le_bytes(to_le_bytes(&samples)), samples);
+    }
+
+    #[test]
+    fn from_le_bytes_drops_trailing_partial_sample() {
+        // Odd number of bytes: the final byte cannot form a full sample and is dropped.
+        assert_eq!(from_le_bytes([0x01, 0x00, 0xff]), vec![1]);
+    }
+
+    #[test]
+    fn from_le_bytes_empty_input() {
+        assert_eq!(from_le_bytes([]), Vec::<i16>::new());
+    }
 }
